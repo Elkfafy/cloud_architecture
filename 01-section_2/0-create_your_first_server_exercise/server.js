eec6797d824ee705cf3dcb371addfafa0115854e
ahmed elkfafy <hmdeslam.adel@gmail.com>
2025-02-24 06:48:05 +0200

    all

--- 01-section_2/0-create_your_first_server_exercise/server.js
+++ 01-section_2/0-create_your_first_server_exercise/server.js
@@ -1,15 +1,17 @@
-const http = require('http')
+const http = require("http")
 
-const server = http.createServer((req, res) => {
-    if (req.method === "GET" && req.url === '/')
-    {
-        res.writeHead(200, {
-            "content-type": "text/plain"
-        })
-        res.end("hello world")
+const server = http.createServer(
+    (req, res) => {
+        if (req.method === 'GET' && req.url === '/')
+        {
+            res.writeHead(200, {
+                "content-type": "text/plain"
+            })
+            res.end("<h1>hello world</h1>")
+        }
     }
-})
+)
 
 server.listen(3333, () => {
-    console.log("server is running in port 3333")
+    console.log("server is running on port 3333")
 })
--- 01-section_2/3-create_your_firt_express_server/server.js
+++ 01-section_2/3-create_your_firt_express_server/server.js
@@ -2,8 +2,13 @@ const express = require('express')
 
 const app = express()
 
+app.use((req, res, next) => {
+    req.body = "hello from delta"
+    next()
+})
+
 app.get('/', (req, res) => {
-    res.send("hello world")
+    res.send("hello, world")
 })
 app.listen(3333, () => {
     console.log("server is running in port 3333")
